2b7241cfe4680ee4f5e3a47864472dc2f4980610
Huang Yuyao <huangyuyao@outlook.com>
2019-08-03 21:51:18 +0800

    redesigned architecture

--- bevutils/__init__.py
+++ bevutils/__init__.py
@@ -1,4 +1,4 @@
-from .perspective_transformer import PerspectiveTransformerLayer
+from .layers.perspective_transformer import PerspectiveTransformerLayer
 
 __all__ = [
     "PerspectiveTransformerLayer",
