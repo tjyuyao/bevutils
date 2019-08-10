fixed a bug in weighting

bevutils/layers/perspective_transformer.py
@@ -62,8 +62,8 @@ class PerspectiveTransformerLayer(nn.Module):
         # gather pixels acoording to `pv_coord`
         x = pv_coord[:,None,:,:,0,0] # with shape (B, 1, Hb, Wb)
         y = pv_coord[:,None,:,:,1,0]
-        x0 = x.to(torch.long).clamp_(0, Wp-2)
-        y0 = y.to(torch.long).clamp_(0, Hp-2)
+        x0 = x.clamp_(0, Wp-2).to(torch.long)
+        y0 = y.clamp_(0, Hp-2).to(torch.long)
         offset_00 = y0 * Wp + x0
         offset_01 = offset_00 + 1
         offset_10 = offset_00 + Wp
@@ -76,7 +76,7 @@ class PerspectiveTransformerLayer(nn.Module):
             torch.gather(pv, -1, offset_11.expand(B, C, Hb, Wb).view(B, C, Hb*Wb))] # pv maps: with shape (B, C, Hb*Wb)
         # combine pv pixels
         x0, x1, y0, y1 = (x - x0.to(self.dtype)), ((x0+1).to(self.dtype) - x), (y - y0.to(self.dtype)), ((y0+1).to(self.dtype) - y)
-        weights = [(x0 * y0), (x0 * y1), (x1 * y0), (x1 * y1)] # weight : with shape (B, 1, Hb, Wb)
+        weights = [(x1 * y1), (x0 * y1), (x1 * y0), (x0 * y0)] # weight : with shape (B, 1, Hb, Wb)
         bvmap = sum([w.expand(B, C, Hb, Wb) * p.view(B, C, Hb, Wb) for w, p in zip(weights, pvmap)]) # bvmap with shape (B, C, Hb, Wb)
         mask = (~((x >= 0) & (x < Wp) & (y >= 0) & (y < Hp))).expand(B, C, Hb, Wb)
         bvmap[mask] = 0.0